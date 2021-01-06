95b3b2d7bcc49b7231b4f06976464ff2f40f77ff
Suleman Anwar <suleman.anwar@alldaypa.com>
2021-01-06 18:12:52 +0000

    changed styling of Search

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,11 @@
-import React, { useState, useContext } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import React, {useState, useContext} from 'react';
+import {Form, Button, Row, Col} from 'react-bootstrap';
 import JobsContext from '../context/Jobs';
+import {faMapMarkerAlt, faMapPin, faSearch} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Search = () => {
-  const { onSearch } = useContext(JobsContext);
+  const {onSearch} = useContext(JobsContext);
 
   const [state, setState] = useState({
     description: '',
@@ -12,7 +14,7 @@ const Search = () => {
   });
 
   const handleInputChange = (event) => {
-    const { name, value } = event.target;
+    const {name, value} = event.target;
     if (name === 'full_time') {
       setState((prevState) => ({
         ...state,
@@ -35,47 +37,43 @@ const Search = () => {
   return (
     <div className="search-section">
       <Form className="search-form" onSubmit={handleSearch}>
-        <Row>
-          <Col>
-            <Form.Group controlId="description">
-              <Form.Control
-                type="text"
-                name="description"
-                value={state.description || ''}
-                placeholder="Enter search term"
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="location">
-              <Form.Control
-                type="text"
-                name="location"
-                value={state.location || ''}
-                placeholder="Enter location"
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Button variant="primary" type="submit" className="btn-search">
-              Search
-            </Button>
-          </Col>
-        </Row>
-        <div className="filters">
-          <Form.Group controlId="full_time">
-            <Form.Check
-              type="checkbox"
-              name="full_time"
-              className="full-time-checkbox"
-              label="Full time only"
-              checked={state.full_time}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-        </div>
+        <FontAwesomeIcon className="icon" icon={faSearch} size="lg"/>
+        <Form.Group controlId="description">
+          <Form.Control
+            type="text"
+            name="description"
+            value={state.description || ''}
+            placeholder="Enter search term"
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <div className="vl"/>
+        <FontAwesomeIcon icon={faMapMarkerAlt} size="lg"/>
+        <Form.Group controlId="location">
+          <Form.Control
+            type="text"
+            name="location"
+            value={state.location || ''}
+            placeholder="Enter location"
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Button className="btn-search" type="submit">
+          Search
+        </Button>
+        {/*<div className="filters">*/}
+        {/*  <Form.Group controlId="full_time">*/}
+        {/*    <Form.Check*/}
+        {/*      type="checkbox"*/}
+        {/*      name="full_time"*/}
+        {/*      className="full-time-checkbox"*/}
+        {/*      label="Full time only"*/}
+        {/*      checked={state.full_time}*/}
+        {/*      onChange={handleInputChange}*/}
+        {/*    />*/}
+        {/*  </Form.Group>*/}
+        {/*</div>*/}
       </Form>
     </div>
   );
