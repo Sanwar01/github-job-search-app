6354edc49b3c8b14939fc87e220ac68a8aeaf608
Suleman Anwar <suleman.anwar@alldaypa.com>
2021-01-08 14:24:28 +0000

    added full time button in search form

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -5,84 +5,81 @@ import {faMapMarkerAlt, faSearch} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Search = () => {
-  const {onSearch} = useContext(JobsContext);
+    const {onSearch} = useContext(JobsContext);
 
-  const [state, setState] = useState({
-    description: '',
-    location: '',
-    full_time: false
-  });
+    const [state, setState] = useState({
+      description: '',
+      location: '',
+      full_time: false
+    });
 
-  const handleInputChange = (event) => {
-    const {name, value} = event.target;
-    if (name === 'full_time') {
-      setState((prevState) => ({
-        ...state,
-        [name]: !prevState.full_time
-      }));
-    } else {
+    const handleInputChange = (event) => {
+      const {name, value} = event.target;
       setState({
         ...state,
         [name]: value
       });
+    };
+
+    const handleFullTimeBtnClick = () => {
+      setState((prevState) => ({
+        ...state,
+        full_time: !prevState.full_time
+      }));
     }
-  };
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    console.log(state);
-    onSearch(state);
-  };
+    const handleSearch = (event) => {
+      event.preventDefault();
+      console.log(state);
+      onSearch(state);
+    };
 
-  return (
-    <div className="search-section">
+    return (
+      <div className="search-section">
 
-      <div className="title">
-        <h1>Search for your dream job</h1>
-        <h3>Finding your new job just got easier</h3>
+        <div className="title">
+          <h1>Search for your dream job</h1>
+          <h3>Finding your new job just got easier</h3>
+        </div>
+
+        <Form className="search-form" onSubmit={handleSearch}>
+          <FontAwesomeIcon id="search-icon" icon={faSearch} fixedWidth/>
+          <Form.Group controlId="description">
+            <Form.Control
+              type="text"
+              name="description"
+              value={state.description || ''}
+              placeholder="Enter search term"
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <div className="vl"/>
+          <FontAwesomeIcon id="location-icon" icon={faMapMarkerAlt} fixedWidth/>
+          <Form.Group controlId="location">
+            <Form.Control
+              type="text"
+              name="location"
+              value={state.location || ''}
+              placeholder="Enter location"
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <div
+            className={state.full_time ? "full-time-btn active" : "full-time-btn"}
+            name="full_time"
+            onClick={handleFullTimeBtnClick}>
+            Full Time
+          </div>
+
+          <button className="btn-search" type="submit">
+            Search
+          </button>
+
+        </Form>
       </div>
-
-      <Form className="search-form" onSubmit={handleSearch}>
-        <FontAwesomeIcon id="search-icon" icon={faSearch} fixedWidth/>
-        <Form.Group controlId="description">
-          <Form.Control
-            type="text"
-            name="description"
-            value={state.description || ''}
-            placeholder="Enter search term"
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-        <div className="vl"/>
-        <FontAwesomeIcon id="location-icon"  icon={faMapMarkerAlt} fixedWidth/>
-        <Form.Group controlId="location">
-          <Form.Control
-            type="text"
-            name="location"
-            value={state.location || ''}
-            placeholder="Enter location"
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-
-        <button className="btn-search" type="submit">
-          Search
-        </button>
-        {/*<div className="filters">*/}
-        {/*  <Form.Group controlId="full_time">*/}
-        {/*    <Form.Check*/}
-        {/*      type="checkbox"*/}
-        {/*      name="full_time"*/}
-        {/*      className="full-time-checkbox"*/}
-        {/*      label="Full time only"*/}
-        {/*      checked={state.full_time}*/}
-        {/*      onChange={handleInputChange}*/}
-        {/*    />*/}
-        {/*  </Form.Group>*/}
-        {/*</div>*/}
-      </Form>
-    </div>
-  );
-};
+    );
+  }
+;
 
 export default Search;
